657eb3d21ee46b20d7752c4d6e8bd32b257e3e86
Greedwy-Blu <greedwyblu@gmail.com>
2023-01-30 14:13:20 -0300

    header-text update

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,18 +3,26 @@ import { Border, HeaderText } from "./styled";
 
 export function Header(){
   return(
-<div className="mx-auto  overflow-hidden ">
+<div className=" mx-auto  overflow-hidden ">
 
 <div className="grid grid-rows-1 grid-flow-col  columns-2">
 
-<div>
-  <a href="">dfsfdfdfs</a>
+<div className="flex justify-around">
+
+
+<div className="flex items-center w-2/5">
+  <a  className="text-left  font-bold text-bege-escuro text-2xl">
+    Hey! I'm gabriel I'm 18  old I'm a stundent  computer science  . I'm addicted to learning about technologies and programming languages and their impact on the world and their elaboration process to achieve a better world for us through technology.
+  </a>
 </div>
 
 
-<div className="flex justify-end mt-20 mr-32 ">
+<div className="mt-20">
 
 <Border/>
+
+</div>
+
 
 </div>
 
