9d668adf7c82663f9412303ed997201d5662cc30
Greedwy-Blu <greedwyblu@gmail.com>
2023-02-14 17:47:21 -0300

    teste do slider

--- src/components/FrontEnd/index.tsx
+++ src/components/FrontEnd/index.tsx
@@ -8,19 +8,28 @@ import image2 from '../../assets/gears.svg'
 
 
 
-const images = [
-  'https://images.unsplash.com/photo-1658171757201-41b9aa2b3651?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
-  'https://images.unsplash.com/photo-1658184145200-3358c3338585?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80',
-  'https://images.unsplash.com/photo-1658186309018-1e3b880acd0c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
-  'https://images.unsplash.com/photo-1658193624919-50e49d7847e3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
-  'https://images.unsplash.com/photo-1572816703439-d8b34c4dc93f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1334&q=80',
-  'https://images.unsplash.com/photo-1658901097742-98954bfd9b4f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1191&q=80',
-  'https://images.unsplash.com/photo-1658901097917-f3e4f3cb5f1f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1192&q=80',
-  'https://images.unsplash.com/photo-1642320009030-ff80041e25ed?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1229&q=80',
-  'https://images.unsplash.com/photo-1658901097893-cfe9605ca208?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1192&q=80',
-  'https://images.unsplash.com/photo-1659039288596-45cf0fbaee51?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
-  'https://images.unsplash.com/photo-1658730335794-c5edd544ccbb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+const sliders = [
+
+
+{
+  title: 'dsdfdsfds',
+  image: 'https://images.unsplash.com/photo-1658171757201-41b9aa2b3651?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
+},
+{
+  title: 'ewfsdda',
+  image: 'https://images.unsplash.com/photo-1658184145200-3358c3338585?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80',
+},
+{
+  title: 'sdafs',
+image: 'https://images.unsplash.com/photo-1658186309018-1e3b880acd0c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
+},
+{
+  title: 'sdasdsa',
+  image: 'https://images.unsplash.com/photo-1658193624919-50e49d7847e3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
+}
+
 ]
+
 
 const variants = {
   initial: direction => {
@@ -61,7 +70,7 @@ export function FrontEnd(){
 
   function nextStep() {
     setDirection(1)
-    if (index === images.length - 1) {
+    if (index === sliders.length - 1) {
       setIndex(0)
       return
     }
@@ -71,11 +80,20 @@ export function FrontEnd(){
   function prevStep() {
     setDirection(-1)
     if (index === 0) {
-      setIndex(images.length - 1)
+      setIndex(sliders.length - 1)
       return
     }
     setIndex(index - 1)
   }
+
+  var byslider = function(object) {
+    return object.image
+  };
+  var bysliderTitle = function(object) {
+    return object.title
+};
+
+console.log(byslider(sliders[0]));
 
 
 return(
@@ -99,15 +117,19 @@ return(
 
 </div>
 
-<div className="flex justify-center">
+<div className="flex justify-center mr-40">
 
-      <div className="translate-x-12 mt-48 ">
-      <a className="text-3xl">Texto exemplo</a>
-      </div>
 
-  <button className='prevButton bg-transparent' onClick={prevStep}>
-        ◀
-  </button>
+<div className="translate-x-12 mt-48 ">
+        <a className="text-3xl font-bold text-bege-escuro-2">{bysliderTitle(sliders[index])}</a>
+        </div>
+
+
+     <button className='prevButton bg-transparent' onClick={prevStep}>
+          ◀
+     </button>
+
+
 
         <AnimatePresence initial={false} custom={direction}>
           <motion.div
@@ -116,7 +138,7 @@ return(
             initial='initial'
             exit='exit'
             className='slides mt-20 '
-            key={images[index]}
+            key={index}
             custom={direction}
           >
 
@@ -124,9 +146,9 @@ return(
 
 
                   <div className="">
-                    <div className="w-[500px] h-[500px] bg-yellow-bege">
+                    <div className="w-[500px] h-[500px] bg-color-17">
 
-                    <img src={images[index]} alt="" className="h-96 w-96"/>
+                    <img src={byslider(sliders[index])} alt="" className="h-96 w-96"/>
 
                     </div>
 
