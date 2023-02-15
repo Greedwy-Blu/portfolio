423cf5655aa2fab1ee2af3b191257f63a294f09b
Greedwy-Blu <greedwyblu@gmail.com>
2023-02-15 18:49:34 -0300

    update

--- src/components/Techs/index.tsx
+++ src/components/Techs/index.tsx
@@ -7,8 +7,9 @@ export function Techs(){
 
 <p className="flex items-center space-x-3 font-bold not-italic">
 
+
+<a className="text-4xl text-bege-escuro-2">BACKEND</a>
   <div className="border-t-2 w-5/6  border-bege-escuro"></div>
-  <a className="text-4xl text-bege-escuro-2">BACKEND</a>
 
 
 
