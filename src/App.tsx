bd4401243e0c15038df005005dca85581ab06851
Greedwy-Blu <greedwyblu@gmail.com>
2023-02-16 23:29:39 -0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import { Navbar } from './components/navbar/';
-import  Header  from './components/about/';
+import  Header  from './components/about';
 import  Footer  from './components/Footer';
-import  FrontEnd  from './components/frontend/';
+import  FrontEnd  from './components/frontend';
 import  Scroll  from './components/scroll';
-import  BackEnd  from './components/backend/';
-import  Techs  from './components/techs/';
+import  BackEnd  from './components/backend';
+import  Techs  from './components/techs';
 import  Contact  from './components/Contact';
 
 function App() {
